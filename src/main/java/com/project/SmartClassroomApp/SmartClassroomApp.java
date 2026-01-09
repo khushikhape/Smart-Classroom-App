37b7befd00e3d724da4752ff05e5093ce38621c7
Kiran 6972 <khapekhushi@gmail.com>
2026-01-09 18:53:13 +0530

    Initial commit

--- src/main/java/com/project/SmartClassroomApp/SmartClassroomApp.java
+++ src/main/java/com/project/SmartClassroomApp/SmartClassroomApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementApplication {
+public class SmartClassroomApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
+		SpringApplication.run(SmartClassroomApp.class, args);
 	}
 
 }
